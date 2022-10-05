52030e1db0212e4109e268350ddc39535ad2d276
llwyd <tom@llwyd.io>
2022-10-05 21:18:41 +0100

    getting the hang of it

--- xmacro/fsm_xmacro.c
+++ xmacro/fsm_xmacro.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 typedef struct
 {
     void (*fn)(void);
 } state_machine_t;
 
+typedef uint32_t signal_t;
+
 typedef enum
 {
     fsm_Handled,
     fsm_Transition,
     fsm_SuperTransition,
-} state_codes_t;
+} state_return_t;
 
 #define ROOT_STATE \
     X( ROOT ) \
@@ -25,13 +29,23 @@ typedef enum
     X( A1 ) \
     X( B0 ) \
 
+#define STATES \
+    X( A, ROOT ) \
+    X( B, ROOT ) \
+    X( A0, A ) \
+    X( A1, A ) \
+    X( B0, A ) \
+
 #define ENUM_(x) STATE_##x
-#define FUNC_(x) State_##x
+#define FUNC_(x) State##x
+
+#define FUNC_RETURN  state_return_t 
+#define FUNC_ARGS ( void )
 
 typedef enum
 {
+    STATE_ROOT,
     #define X(a) ENUM_(a),
-        ROOT_STATE
         PARENT_STATES
         SUB_STATES
     #undef X
@@ -40,24 +54,64 @@ typedef enum
 state_t;
 
 
-#define X(a) void FUNC_(a) (void);
-    PARENT_STATES
-    SUB_STATES
+#define X(a,b) FUNC_RETURN FUNC_(a) FUNC_ARGS;
+    STATES
 #undef X
 
-
-void StateA( void )
+const state_t parent_lookup[ ] =
 {
+    #define X(a, b) [ ENUM_(a) ] = ENUM_(b),
+        STATES
+    #undef X
+};
 
+state_return_t (*func_lookup[ ])(void) =
+{
+    [ STATE_ROOT ] = NULL,
+    #define X(a, b) [ ENUM_(a) ] = FUNC_(a),
+        STATES
+    #undef X
+};
+
+state_return_t StateB0( void )
+{
+    printf("Func: %s\n", __func__);
+    state_return_t ret = fsm_Handled;
+    return ret;
 }
 
-void StateB( void )
+state_return_t StateA1( void )
 {
+    printf("Func: %s\n", __func__);
+    state_return_t ret = fsm_Handled;
+    return ret;
+}
 
+state_return_t StateA0( void )
+{
+    printf("Func: %s\n", __func__);
+    state_return_t ret = fsm_Handled;
+    return ret;
+}
+
+state_return_t StateA( void )
+{
+    printf("Func: %s\n", __func__);
+    state_return_t ret = fsm_Handled;
+    return ret;
+}
+
+state_return_t StateB( void )
+{
+    printf("Func: %s\n", __func__);
+    state_return_t ret = fsm_Handled;
+    return ret;
 }
 
 int main( void )
 {
     printf("X-Macro FSM Example\n");
+
+    func_lookup[STATE_A]();
     return 0;
 }
