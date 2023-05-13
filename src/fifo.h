58af6fc211b001a39ceaded7a048475b8083b685
llwyd <tom@llwyd.io>
2023-05-13 22:01:24 +0100

    Dequeue function

--- src/fifo.h
+++ src/fifo.h
@@ -14,6 +14,8 @@ typedef struct
 }
 fifo_base_t;
 
+/* TODO: Critical Sections */
+
 #define FIFO_PASS_PTR(x) x * fifo
 #define FIFO_FUNC_INIT(x) FIFO_Init##x
 #define FIFO_FUNC_DEQ(x) FIFO_DEQ##x
@@ -28,12 +30,28 @@ fifo_base_t;
     } \
     FIFO_TYPE; \
     \
+    \
     void FIFO_FUNC_INIT(NAME) (FIFO_PASS_PTR(FIFO_TYPE)) \
     { \
         fifo->base.max = LEN; \
         fifo->base.fill = 0U; \
         fifo->base.r_index = 0U; \
         fifo->base.w_index = 0U; \
-    } 
+    } \
+    \
+    DATA_TYPE FIFO_FUNC_DEQ(NAME) (FIFO_PASS_PTR(FIFO_TYPE)) \
+    { \
+        assert( fifo != NULL ); \
+        assert( fifo->base.fill > 0U ); \
+        \
+        DATA_TYPE ret = fifo->queue[ fifo->base.r_index ]; \
+        fifo->base.r_index++; \
+        fifo->base.fill--; \
+        fifo->base.r_index = ( fifo->base.r_index & ( LEN - 1U ) ); \
+        \
+        return ret; \
+    }  
+
+    
 
 #endif /* FIFO_H_ */
