aac3ab1057ad3ca6ebddaa2d6e072c8e9924222c
llwyd <tom@llwyd.io>
2024-05-02 22:54:24 +0100

    push tests for heap

--- src/heap_base.c
+++ src/heap_base.c
@@ -1,5 +1,16 @@
 #include "heap_base.h"
 #include <string.h>
+#include <stdio.h>
+
+static void swap( uint32_t * a, uint32_t * b )
+{
+    assert(a != NULL);
+    assert(b != NULL);
+
+    uint32_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 extern void Heap_Init(heap_t * heap)
 {
@@ -7,5 +18,36 @@ extern void Heap_Init(heap_t * heap)
     heap->max = HEAP_LEN;
     memset(heap->heap, 0x00, sizeof(uint32_t) * HEAP_LEN);
 }
-extern void Heap_Push(heap_t * heap, uint32_t data);
+
+extern bool Heap_IsFull(heap_t * heap)
+{
+    return (heap->fill < heap->max);
+}
+
+extern void Heap_Push(heap_t * heap, uint32_t data)
+{
+    (void)heap;
+    (void)data;
+    
+    assert(heap != NULL);
+    assert(heap->fill < heap->max);
+
+    /* Add the value to the bottom of the heap */
+    heap->heap[heap->fill] = data;
+
+    uint32_t idx = heap->fill;
+
+    /* Swim through the heap to ensure heap order is correct */
+    while( idx > 0U )
+    {
+        uint32_t parent = (idx - 1) >> 1U;
+        if( heap->heap[idx] < heap->heap[parent] )
+        {
+            swap(&heap->heap[idx], &heap->heap[parent]);
+        }
+        idx = parent;
+    }
+    heap->fill++; 
+}
+
 extern uint32_t Heap_Pop(heap_t * heap);
--- tests/heap_tests.c
+++ tests/heap_tests.c
@@ -14,7 +14,50 @@ static void test_Heap_Init(void)
 
 }
 
+static void test_Heap_Push(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    
+    TEST_ASSERT_EQUAL( 1U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[0] );
+}
+
+static void test_Heap_Push2Gtr(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 11U);
+    
+    TEST_ASSERT_EQUAL( 2U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 11U, heap.heap[1] );
+}
+
+static void test_Heap_Push2Less(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 9U);
+    
+    TEST_ASSERT_EQUAL( 2U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 9U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[1] );
+}
+
 extern void HeapTestSuite(void)
 {
     RUN_TEST(test_Heap_Init);
+    RUN_TEST(test_Heap_Push);
+    RUN_TEST(test_Heap_Push2Gtr);
+    RUN_TEST(test_Heap_Push2Less);
 }
