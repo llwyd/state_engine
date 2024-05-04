2d33e17280edf3db24f596ef4547393c5db0c76f
llwyd <tom@llwyd.io>
2024-05-04 20:00:51 +0100

    more push tests

--- tests/heap_tests.c
+++ tests/heap_tests.c
@@ -54,10 +54,78 @@ static void test_Heap_Push2Less(void)
     TEST_ASSERT_EQUAL( 10U, heap.heap[1] );
 }
 
+static void test_Heap_Push3GL(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 11U);
+    Heap_Push(&heap, 9U);
+    
+    TEST_ASSERT_EQUAL( 3U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 9U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 11U, heap.heap[1] );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[2] );
+}
+
+static void test_Heap_Push3GG(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 11U);
+    Heap_Push(&heap, 11U);
+    
+    TEST_ASSERT_EQUAL( 3U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 11U, heap.heap[1] );
+    TEST_ASSERT_EQUAL( 11U, heap.heap[2] );
+}
+
+static void test_Heap_Push3LG(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 9U);
+    Heap_Push(&heap, 11U);
+    
+    TEST_ASSERT_EQUAL( 3U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 9U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[1] );
+    TEST_ASSERT_EQUAL( 11U, heap.heap[2] );
+}
+
+static void test_Heap_Push3LL(void)
+{
+    heap_t heap;
+    Heap_Init(&heap);
+
+    Heap_Push(&heap, 10U);
+    Heap_Push(&heap, 9U);
+    Heap_Push(&heap, 8U);
+    
+    TEST_ASSERT_EQUAL( 3U, heap.fill );
+    TEST_ASSERT_EQUAL( HEAP_LEN, heap.max );
+    TEST_ASSERT_EQUAL( 8U, heap.heap[0] );
+    TEST_ASSERT_EQUAL( 10U, heap.heap[1] );
+    TEST_ASSERT_EQUAL( 9U, heap.heap[2] );
+}
+
 extern void HeapTestSuite(void)
 {
     RUN_TEST(test_Heap_Init);
     RUN_TEST(test_Heap_Push);
     RUN_TEST(test_Heap_Push2Gtr);
     RUN_TEST(test_Heap_Push2Less);
+    RUN_TEST(test_Heap_Push3GL);
+    RUN_TEST(test_Heap_Push3GG);
+    RUN_TEST(test_Heap_Push3LG);
+    RUN_TEST(test_Heap_Push3LL);
 }
